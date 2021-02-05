df5676f4b854866373af8740c457f7338299ba8f
shijunlin-miaozi <74685676+shijunlin-miaozi@users.noreply.github.com>
2021-02-05 23:33:05 +0100

    update

--- E19_random_word.py
+++ E19_random_word.py
@@ -1,5 +1,4 @@
 # >>>>>>>>>>>>>>>>>>>>>>>> WORK IN PROGRESS >>>>>>>>>>>>>>>>>>>>>>>>>>
-
 # Think Python 2nd Edition book_Ch13.data structure selection_p126
 
 # # Exercise 13.5. Write a function named choose_from_hist that takes a histogram as defined in
@@ -9,16 +8,41 @@
 # >>> hist = histogram(t)
 # >>> hist
 # {'a': 2, 'b': 1}
+# To choose an element from a sequence at random, you can use choice:
+# >>> t = [1, 2, 3]
+# >>> random.choice(t)
+# 2
+# >>> random.choice(t)
+# 3
 # your function should return 'a' with probability 2/3 and 'b' with probability 1/3.
 
+import random
+from E18_word_freq import book_to_word_hist
 
+def make_histogram(word):
+	hist = {}
+	for letter in word:
+		hist[letter] = hist.setdefault(letter, 0) + 1
+	return hist
+
+def choose_from_hist(hist):
+	letter_list = []
+	for letter, freq in hist.items():
+		letter_list.extend(letter * freq)
+	return random.choice(letter_list)
+
+''' above method is ok for analyzing letter histogram from words (short length). It will be very expensive 
+if to analyze word histogram from a book (eg 7k unique words). below is an alternative: '''
+
+def random_word(hist):
 
 
 
 if __name__ == '__main__':
-	pass
 
+	hist = make_histogram('apple')
+	print(choose_from_hist(hist))
 
+	hist_austen = book_to_word_hist('Pride_and_Prejudice_by_Jane_Austen.txt', 167)
 
 # >>>>>>>>>>>>>>>>>>>>>>>> WORK IN PROGRESS >>>>>>>>>>>>>>>>>>>>>>>>>>
-	
