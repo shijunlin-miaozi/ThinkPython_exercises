b72da30825c022a5c2f764ba290ea7c49acfb53c
shijunlin-miaozi <74685676+shijunlin-miaozi@users.noreply.github.com>
2021-02-06 13:17:18 +0100

    Update E19_random_word.py

--- E19_random_word.py
+++ E19_random_word.py
@@ -1,4 +1,3 @@
-# >>>>>>>>>>>>>>>>>>>>>>>> WORK IN PROGRESS >>>>>>>>>>>>>>>>>>>>>>>>>>
 # Think Python 2nd Edition book_Ch13.data structure selection_p126
 
 # # Exercise 13.5. Write a function named choose_from_hist that takes a histogram as defined in
@@ -17,6 +16,7 @@
 # your function should return 'a' with probability 2/3 and 'b' with probability 1/3.
 
 import random
+from bisect import bisect
 from E18_word_freq import book_to_word_hist
 
 def make_histogram(word):
@@ -31,11 +31,19 @@ def choose_from_hist(hist):
 		letter_list.extend(letter * freq)
 	return random.choice(letter_list)
 
+
 ''' above method is ok for analyzing letter histogram from words (short length). It will be very expensive 
-if to analyze word histogram from a book (eg 7k unique words). below is an alternative: '''
+if to analyze word histogram from a book (need to make list containing e.g. > 120k words). below is an alternative
+using cummulative sum of freq to keep list length to num of unique words (e.g. < 7k)'''
+
 
 def random_word(hist):
-
+	words = list(hist.keys())
+	cum_freqs = [hist[words[0]]]
+	for i in range(1, len(words)):
+		cum_freqs.append(hist[words[i]] + cum_freqs[i - 1])
+	index = bisect(cum_freqs, random.randint(0, cum_freqs[-1] - 1))
+	return words[index]
 
 
 if __name__ == '__main__':
@@ -44,5 +52,4 @@ if __name__ == '__main__':
 	print(choose_from_hist(hist))
 
 	hist_austen = book_to_word_hist('Pride_and_Prejudice_by_Jane_Austen.txt', 167)
-
-# >>>>>>>>>>>>>>>>>>>>>>>> WORK IN PROGRESS >>>>>>>>>>>>>>>>>>>>>>>>>>
+	print(random_word(hist_austen))
