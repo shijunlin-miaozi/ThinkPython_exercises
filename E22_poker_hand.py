2e3a95206e490e2bdc9a217ad93afd9ee9e41b68
shijunlin <miaozilsj@gmail.com>
2021-08-26 15:45:56 +0200

    Update E22_poker_hand.py

--- E22_poker_hand.py
+++ E22_poker_hand.py
@@ -49,13 +49,48 @@ from E22_card import Hand, Deck
 class PokerHand(Hand):
     """Represents a poker hand."""
 
-    def suit_hist(self):
-        """Builds a histogram of the suits that appear in the hand.
-        Stores the result in attribute suits.
+    def make_hist(self):
+        """Builds a histogram of the suits, ranks and rank_freq that appear in the hand.
+        Stores the result in attribute suits, ranks and rank_freq.
         """
-        self.suits = {}
+        self.suits, self.ranks, self.rank_freq = {}, {}, []
         for card in self.cards:
-            self.suits[card.suit] = self.suits.setdefault(card.suit, 0) + 1
+            for d, key in zip((self.suits, self.ranks), (card.suit, card.rank)):
+                d[key] = d.setdefault(key, 0) + 1
+        self.rank_freq = sorted(list(self.ranks.values()), reverse=True)
+
+
+    def compare_rank_freq(self, *reqs):
+        """check whether hand's rank frequencies meet requirements"""
+        for req, have in zip(reqs, self.rank_freq):
+            if have < req:
+                return False
+        return True
+
+
+    def has_pair(self):
+        """ two cards with the same rank """
+        return self.compare_rank_freq(2)
+
+
+    def has_2pair(self):
+        """ two pairs of cards with the same rank"""
+        return self.compare_rank_freq(2, 2)
+
+
+    def has_3ofakind(self):
+        """ three cards with the same rank"""
+        return self.compare_rank_freq(3)
+
+
+    def has_4ofakind(self):
+        """ four cards with the same rank"""
+        return self.compare_rank_freq(4)
+
+
+    def has_fullhouse(self):
+        """ three cards with one rank, two cards with another"""
+        return self.compare_rank_freq(3, 2)
 
 
     def has_flush(self):
@@ -63,113 +98,53 @@ class PokerHand(Hand):
         Returns True if the hand has a flush, False otherwise.      
         Note that this works correctly for hands with more than 5 cards.
         """
-        self.suit_hist()
         for val in self.suits.values():
             if val >= 5:
                 return True
         return False
 
 
-    def rank_hist(self):
-            """Builds a histogram of the ranks that appear in the hand.
-            Stores the result in attribute ranks.
-            """
-            self.ranks = {}
-            for card in self.cards:
-                self.ranks[card.rank] = self.ranks.setdefault(card.rank, 0) + 1
-
-
-    def has_pair(self):
-        """ two cards with the same rank 
-        """
-        self.rank_hist()
-        for val in self.ranks.values():
-            if val >= 2:
-                return True
-        return False
-
-
-    def has_2pair(self):
-        """ two pairs of cards with the same rank
-        """
-        self.rank_hist()
-        ctr = 0
-        for val in self.ranks.values():
-            if val >= 2:
-                ctr += 1
-                if ctr == 2:
-                    return True
-        return False
-
-
-    def has_3ofakind(self):
-        """ three cards with the same rank
-        """
-        self.rank_hist()
-        for val in self.ranks.values():
-            if val >= 3:
-                return True
-        return False
-
-
     def has_straight(self):
-        """ five cards with ranks in sequence (aces can be high or low, so Ace-2-3-4-5 is a straight and so is 10-Jack-Queen-King-Ace, but Queen-King-Ace-2-3 is not.)
-        """
-        self.rank_hist()
-        lt = sorted(self.ranks.keys())
-        if len(lt) >= 5:
-            if set(lt) >= {10, 11, 12, 13, 1}:
+        """ five cards with ranks in sequence (aces can be high or low, so Ace-2-3-4-5 is a straight and so is 10-Jack-Queen-King-Ace, but Queen-King-Ace-2-3 is not.)"""
+        ctr, uni_rank = 0, self.ranks.keys()
+        if len(uni_rank) >= 5:
+            if set(uni_rank) >= {10, 11, 12, 13, 1}:
                 return True
-            for i in range(len(lt) - 4):
-                if lt[i:i+5] == [lt[i]+n for n in range(5)]:
-                    return True
-        return False
-
-
-    def has_fullhouse(self):
-        """ three cards with one rank, two cards with another
-        """
-        self.rank_hist()
-        lt = [val for val in self.ranks.values() if val >= 2]
-        if len(lt) >= 2 and (3 in lt or 4 in lt):
-            return True
-        return False
-
-
-    def has_4ofakind(self):
-        """ four cards with the same rank
-        """
-        self.rank_hist()
-        for val in self.ranks.values():
-            if val >= 4:
-                return True
+            for i in range(1, 14):
+                if self.ranks.get(i, 0):
+                    ctr += 1
+                    if ctr == 5:
+                        return True
+                else:
+                    ctr = 0
         return False
 
 
     def has_straightflush(self):
-        """ five cards in sequence (as defined above) and with the same suit
-        """
-        self.suit_hist()
-        flush = PokerHand()
-        for key, val in self.suits.items():
-            if val >= 5:
+        """ five cards in sequence (as defined above) and with the same suit"""
+        hand = PokerHand()
+        for suit, freq in self.suits.items():
+            if freq >= 5:
                 for card in self.cards:
-                    if card.suit == key:
-                        flush.add_card(card)
-                if flush.has_straight():
+                    if card.suit == suit:
+                        hand.add_card(card)
+                hand.make_hist()
+                if hand.has_straight():
                     return True
         return False
 
 
     def classify(self):
-        res, labels = [], ["pair", "2pair", "3ofakind", "straight", 
-                            "flush", "fullhouse", "4ofakind", "straightflush"]
-        for value, check in zip((range(8)), 
-                            (self.has_pair(), self.has_2pair(), self.has_3ofakind(), self.has_straight(), 
-                            self.has_flush(), self.has_fullhouse(), self.has_4ofakind(), self.has_straightflush())):
-            if check:
-                res.append(value)
-        self.label = labels[max(res)] if res else None
+        labels = ['straightflush', '4ofakind', 'fullhouse', 'flush', 
+                    'straight', '3ofakind', '2pair', 'pair'] 
+        self.make_hist()
+        for label in labels:
+            f = getattr(self, 'has_' + label)
+            if f():
+                self.label = label
+                return
+            else:
+                self.label = None
 
 
 def calc_probability(deck_num, card_num):
@@ -177,58 +152,24 @@ def calc_probability(deck_num, card_num):
     for i in range(deck_num):
         deck = Deck()
         deck.shuffle()
-        round_num = divmod(52, card_num)[0]
-        for i in range(round_num):
+        hand_num = divmod(52, card_num)[0]
+        for i in range(hand_num):
             hand = PokerHand()
             deck.move_cards(hand, card_num)
             hand.sort()
             hand.classify()
             res[hand.label] = res.setdefault(hand.label, 0) + 1
     for key, val in res.items():
-        res[key] = val / (deck_num * round_num)
+        res[key] = val / (deck_num * hand_num)
     return sorted(res.items(), key=lambda x:x[1], reverse=True)
 
 
 if __name__ == '__main__':
+
     for card_num in (5, 7):
         res = calc_probability(10000, card_num)
+
         print(f"probabilities for {card_num}-card poker hands:")
         for label, prob in res:
             print(f"\t{label}\t{prob: .2%}")
         print()
-
-
-
-    # make a deck
-    # deck = Deck()
-    # deck.shuffle()
-    # cards =[Card(1, 13),
-    #         Card(1, 12),
-    #         Card(1, 11),
-    #         Card(1, 10),
-    #         Card(1, 1),
-    #         Card(2, 10),
-    #         Card(3, 4),
-    #         ]
-    # for card in cards:
-        # print(card)
-        # deck.add_card(card)
-
-    # deal the cards and classify the hands
-    # for i in range(7):
-    #     hand = PokerHand()
-    #     deck.move_cards(hand, 7)
-
-    #     hand.sort()
-    #     print(hand)
-    #     print('\t0) has_pair:', hand.has_pair())
-    #     print('\t1) has_2pair:', hand.has_2pair())
-    #     print('\t2) has_3ofakind:', hand.has_3ofakind())
-    #     print('\t3) has_straight:', hand.has_straight())
-    #     print('\t4) has_flush:', hand.has_flush())
-    #     print('\t5) has_fullhouse:', hand.has_fullhouse())
-    #     print('\t6) has_4ofakind:', hand.has_4ofakind())
-    #     print('\t7) has_straightflush:', hand.has_straightflush())
-    #     print('')
-
-    # hand.classify()
